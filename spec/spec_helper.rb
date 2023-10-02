6124814f5e414e4901281f3ad779fac7df6ceb2d
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-03 00:37:12 +0100

    Configure RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,13 @@
-# frozen_string_literal: true
-
 require "id3"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  Kernel.srand(config.seed)
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.order = :random
+
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
+
+  config.default_formatter = :doc if config.files_to_run.one?
 end
