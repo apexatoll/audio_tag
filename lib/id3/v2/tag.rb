988a0d3e7843a1289f0830371ad94fd4002d0edf
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-10 19:07:08 +0100

    Create tag class

---
+++ lib/id3/v2/tag.rb
@@ -0,0 +1,29 @@
+module ID3
+  module V2
+    class Tag < StreamSection
+      attr_reader :header, :frames
+
+      def initialize(stream)
+        @header = TagHeader.new(stream)
+
+        super(stream)
+      end
+
+      private
+
+      def end_of_tag?
+        stream.pos >= last_pos || read_byte.zero?
+      end
+
+      def size
+        header.tag_size
+      end
+
+      def extract!
+        @frames = [].tap do |frames|
+          frames << Frame.new(stream) until end_of_tag?
+        end
+      end
+    end
+  end
+end
---
+++ spec/id3/v2/tag_spec.rb
@@ -0,0 +1,91 @@
+RSpec.describe ID3::V2::Tag do
+  subject(:tag) { described_class.new(stream) }
+
+  let(:stream) { StringIO.new(string) }
+
+  let(:string) do
+    "ID3\x04\x00\x00\x00\x00\x00W" \
+    "TALB\x00\x00\x00\v\x00\x00\x03Some Album" \
+    "TIT2\x00\x00\x00\v\x00\x00\x03Some Track" \
+    "TPE1\x00\x00\x00\f\x00\x00\x03Some Artist" \
+    "TDRL\x00\x00\x00\x05\x00\x00\x001996" \
+    "\x00\x00\x00\x00\x00\x00\x00\x00"
+  end
+
+  describe "#header" do
+    subject(:header) { tag.header }
+
+    let(:expected_attributes) do
+      {
+        tag_size: 87,
+        unsynchronised?: false,
+        extended?: false,
+        experimental?: false
+      }
+    end
+
+    before { tag }
+
+    it "returns a tag header" do
+      expect(header).to be_an(ID3::V2::TagHeader)
+    end
+
+    it "sets the expected attributes" do
+      expect(header).to have_attributes(**expected_attributes)
+    end
+
+    it "does not advance the stream further" do
+      expect { header }.not_to change { stream.pos }.from(97)
+    end
+  end
+
+  describe "#frames" do
+    subject(:frames) { tag.frames }
+
+    before { tag }
+
+    it "returns an array of frames" do
+      expect(frames).to all be_an(ID3::V2::Frame)
+    end
+
+    it "contains the expected number of frames" do
+      expect(frames.count).to eq(4)
+    end
+
+    it "does not advance the stream further" do
+      expect { frames }.not_to change { stream.pos }.from(97)
+    end
+
+    describe "first frame" do
+      subject(:frame) { frames[0] }
+
+      it "has the expected attributes" do
+        expect(frame.to_h).to eq(TALB: "\x03Some Album")
+      end
+    end
+
+    describe "second frame" do
+      subject(:frame) { frames[1] }
+
+      it "has the expected attributes" do
+        expect(frame.to_h).to eq(TIT2: "\x03Some Track")
+      end
+    end
+
+    describe "third frame" do
+      subject(:frame) { frames[2] }
+
+      it "has the expected attributes" do
+        expect(frame.to_h).to eq(TPE1: "\x03Some Artist")
+      end
+    end
+
+    describe "fourth frame" do
+      subject(:frame) { frames[3] }
+
+      it "has the expected attributes" do
+        expect(frame.to_h).to eq(TDRL: "\x001996")
+      end
+    end
+  end
+end
