215be3929812797dd848708c961580b0ef7740a9
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-11 09:22:46 +0100

    Add acceptance spec helper

---
+++ spec/support/acceptance_helper.rb
@@ -0,0 +1,25 @@
+RSpec.configure do |config|
+  config.define_derived_metadata(%r{spec/acceptance/}) do |metadata|
+    metadata[:acceptance] = true
+  end
+end
+
+module AcceptanceHelper
+  def tags
+    ID3::V2::Tag.from_file(path).to_h
+  end
+
+  def example(name)
+    File.join(File.dirname(File.expand_path(__dir__)), "examples", name)
+  end
+
+  def example_name
+    RSpec.current_example.metadata[:full_description].strip
+  end
+
+  RSpec.shared_examples :parses_tags do
+    it "returns the expected tags" do
+      expect(tags).to eq(expected_tags)
+    end
+  end
+end
